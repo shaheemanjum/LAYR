1959be3e7564054af4334dbde931771d0dfefbfe
Shaheem Anjum <Shaheem.Anjum@rsystems.com>
2022-10-10 11:03:47 +0530

    final commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'bmd3y2',
+ 
+  defaultCommandTimeout: 6000,
   e2e: {
+    baseUrl: 'https://app.vwo.com/#/login',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+import locators from '../locators/locators.json'
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +24,69 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('Login', (email, password, dashBoardHeaderMsg) => {
+
+    cy.get(locators.Login.email).type(email)
+    cy.get(locators.Login.pwd).type(password)
+    cy.get(locators.Login.signInButton).click()
+    cy.get(locators.DashBoard.headerMsg, { timeout: 15000 }).should('be.visible').then((ele) => {
+
+        expect(ele.text().trim()).equal(dashBoardHeaderMsg)
+    })
+
+})
+
+Cypress.Commands.add('verifyDashBoardNavItems', (navArray) => {
+
+    navArray.forEach(element => {
+
+        cy.contains(element).should('be.visible')
+    });
+
+
+})
+
+Cypress.Commands.add('createHypothesis', (data) => {
+    cy.get(locators.Hypotheses.expectThatInputBox).type(data.expectThat)
+    cy.get(locators.Hypotheses.willAddressInputBox).type(data.willAddress)
+    cy.get(locators.Hypotheses.labelsInputBox).type(data.Label)
+    if (data.Label.includes('Label1')) {
+        cy.get(locators.Hypotheses.confidenceRatingCgart).first().click()
+        cy.get(locators.Hypotheses.importanceRatingCgart).first().click()
+        cy.get(locators.Hypotheses.easeRatingCgart).first().click()
+    }
+
+    else if (data.Label.includes('Label2')) {
+        cy.get(locators.Hypotheses.confidenceRatingCgart).first().next().click()
+        cy.get(locators.Hypotheses.importanceRatingCgart).first().next().click()
+        cy.get(locators.Hypotheses.easeRatingCgart).first().next().click()
+
+    }
+    else {
+        cy.get(locators.Hypotheses.confidenceRatingCgart).first().next().next().click()
+        cy.get(locators.Hypotheses.importanceRatingCgart).first().next().next().click()
+        cy.get(locators.Hypotheses.easeRatingCgart).first().next().next().click()
+    }
+
+    cy.get(locators.Hypotheses.createHypoButton).click({ force: true })
+})
+
+Cypress.Commands.add('verifyHypothesesSearch', () => {
+    cy.get(locators.DashBoard.plan).click()
+    cy.get(locators.DashBoard.hypotheses, { timeout: 12000 }).click()
+    cy.get(locators.Hypotheses.hypothesesHeader).should('be.visible')
+    cy.wait(2000)
+    cy.get(locators.Hypotheses.listViewLink,{timeout:10000}).click({force:true})
+    cy.wait(2000)
+    cy.get(locators.Hypotheses.sortByDropDown).click()
+    cy.contains('Score').click()
+    cy.get(locators.Hypotheses.datePicker).click()
+    cy.get(locators.Hypotheses.selectedDate).click()
+    cy.get(locators.Hypotheses.calenderFilterButton).click()
+    cy.get(locators.Hypotheses.filterByBacklog).click()
+    cy.wait(2000)
+    cy.get(locators.Hypotheses.hypothesesList).should('be.visible')
+
+
+})
